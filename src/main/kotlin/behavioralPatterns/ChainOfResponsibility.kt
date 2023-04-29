9a13dba47887b21e245bed1125ea684b26892a49
박명수 <devmyoungsoo2@gmail.com>
2023-04-29 17:07:22 +0900

    책임 연쇄 패턴

---
+++ src/main/kotlin/behavioralPatterns/ChainOfResponsibility.kt
@@ -0,0 +1,61 @@
+package behavioralPatterns
+
+data class BankTransaction(val amount: Double)
+
+abstract class ApprovalHandler()  {
+    private var nextHandler: ApprovalHandler? = null
+
+    fun setNextHandler(handler: ApprovalHandler) {
+        nextHandler = handler
+    }
+
+    fun approve(transaction: BankTransaction) {
+        if (canApprove(transaction)) {
+            processApproval(transaction)
+        } else {
+            nextHandler?.approve(transaction)
+        }
+    }
+
+    abstract fun canApprove(transaction: BankTransaction): Boolean
+
+    abstract fun processApproval(transaction: BankTransaction)
+}
+
+class LowLevelManager : ApprovalHandler() {
+    override fun canApprove(transaction: BankTransaction): Boolean = transaction.amount < 100
+
+    override fun processApproval(transaction: BankTransaction) {
+        println("Low-level manager approved transaction for ${transaction.amount}")
+    }
+}
+
+class MiddleLevelManager : ApprovalHandler() {
+    override fun canApprove(transaction: BankTransaction): Boolean = transaction.amount >= 100 && transaction.amount < 500
+
+    override fun processApproval(transaction: BankTransaction) {
+        println("Middle-level manager approved transaction for ${transaction.amount}")
+    }
+}
+
+class HighLevelManager : ApprovalHandler() {
+    override fun canApprove(transaction: BankTransaction): Boolean = transaction.amount >= 500
+
+    override fun processApproval(transaction: BankTransaction) {
+        println("High-level manager approved transaction for ${transaction.amount}")
+    }
+}
+
+fun main() {
+    val lowLevelManager = LowLevelManager()
+    val middleLevelManager = MiddleLevelManager()
+    val highLevelManager = HighLevelManager()
+
+    lowLevelManager.setNextHandler(middleLevelManager)
+    middleLevelManager.setNextHandler(highLevelManager)
+
+    lowLevelManager.approve(BankTransaction(amount = 50.0, processCount = 0))
+    lowLevelManager.approve(BankTransaction(amount = 250.0, processCount = 0))
+    lowLevelManager.approve(BankTransaction(amount = 500.0, processCount = 0))
+    lowLevelManager.approve(BankTransaction(amount = 1500.0, processCount = 0))
+}
